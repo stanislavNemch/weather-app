b02d4ac8f2488d430a2487cf780973af0e542710
Stanislav Nemchenko <stas.buka@gmail.com>
2025-06-19 02:57:58 +0300

    comment consol

--- src/js/render-functions.js
+++ src/js/render-functions.js
@@ -44,7 +44,7 @@ const weatherIcons = {
 };
 
 function getWeatherIconImg(iconCode) {
-  console.log('Icon code:', iconCode);
+  // console.log('Icon code:', iconCode);
   const src = weatherIcons[iconCode] || 'img/icon/default.png';
   return `<img src="${src}" width="48" height="48" alt="weather icon">`;
 }
